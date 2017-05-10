a92b7e01e937943331d1e000bc94d1bde3277d59
Dereje Kitaw <dkitaw@solareworld.local>
2017-05-10 10:47:08 -0400

    get notified if successfull connected or if a connection error occurs

--- getting-started.js
+++ getting-started.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+
+//connect to the test database running on localhost.
+mongoose.connect('mongodb://localhost/test'); // a pending 
+
+ 
+// get notified if successfull connected or if a connection error occurs
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});

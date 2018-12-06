1cb5796767f77832bbea2301c8c03baddfd2f124
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-12-05 21:39:00 -0500

    router factorized

---
+++ 02-vr5/03/index.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const db = 'mongodb://localhost/expressdemo';
+mongoose.connect(db).then(() => {
+  console.log(`Connected to ${db}`);
+});
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+
+require("./startup/routes")(app);
+
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () =>
+  console.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;
